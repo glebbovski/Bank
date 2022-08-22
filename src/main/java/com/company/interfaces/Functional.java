8edd2045f37e6b0f84e9c173533cd39dff6ad688
Gleb Chekmezov <gleeebkachek@gmail.com>
2022-08-22 19:19:57 +0300

    remarks 22-08-2022

--- src/main/java/com/company/interfaces/Functional.java
+++ src/main/java/com/company/interfaces/Functional.java
@@ -1,7 +1,7 @@
 package com.company.interfaces;
 
 import com.company.exceptions.WrongNameOrSurnameException;
-
+@FunctionalInterface
 public interface Functional<T, N> {
     N convert(T t) throws WrongNameOrSurnameException;
 }
